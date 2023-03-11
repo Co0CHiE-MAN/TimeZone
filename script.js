70d0453aaea2c637fc074e7b03c984925c978fce
Co0CHiE-MAN <kalindugunathilake123@gmail.com>
2023-03-11 21:00:42 +0530

    Weather Info Display

--- script.js
+++ script.js
@@ -3,18 +3,18 @@ function updateTime() {
     var colomboTime = new Date().toLocaleTimeString("en-US", { timeZone: "Asia/Colombo" });
     // Get the current time in Melbourne
     var melbourneTime = new Date().toLocaleTimeString("en-US", { timeZone: "Australia/Melbourne" });
-
+    // Get the current time in Adelaide
     var adelaideTime = new Date().toLocaleTimeString("en-US", { timeZone: "Australia/Adelaide" });
-
+    // Get the current time in London
     var ukTime = new Date().toLocaleTimeString("en-US", { timeZone: "Europe/London" });
 
     // Get the current time in Colombo
     var colomboDate = new Date().toLocaleDateString("en-US", { timeZone: "Asia/Colombo" });
     // Get the current time in Melbourne
     var melbourneDate = new Date().toLocaleDateString("en-US", { timeZone: "Australia/Melbourne" });
-
+    // Get the current time in Adelaide
     var adelaideDate = new Date().toLocaleDateString("en-US", { timeZone: "Australia/Adelaide" });
-
+    // Get the current time in London
     var ukDate = new Date().toLocaleDateString("en-US", { timeZone: "Europe/London" });
 
     // Update the time on the webpage
@@ -29,3 +29,24 @@ function updateTime() {
 }
 
 setInterval(updateTime, 1000); // Update the time every second
+
+const countries = ["LK", "AU", "AU", "UK"]
+const cities = ["colombo", "melbourne", "adelaide", "london"];
+const divIds = ["cmb", "mel", "adel", "uk"];
+
+const requestApi = (city, country, divId) => {
+    const apikey = "00278ddfb583d66cf4c1b202aeb0b1e2"
+    let api = `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${apikey}`;
+    fetch(api).then(response => response.json()).then(result => weatherDetails(result, divId));
+}
+
+const weatherDetails = (info,divId) => {
+    let tempCelcius = info.main.temp - 273.15  
+    document.getElementById(divId).innerText = `${Math.round(tempCelcius)}°C`;
+}
+const weatherLoop = (cities) => {
+    for (num in cities) {
+        requestApi(cities[num], countries[num], divIds[num])   
+    }
+}
+weatherLoop(cities)
